e3392922d569645f985c66290b79b243e41cfd81
captaindeng <captaindeng12138@gmail.com>
2024-02-19 11:39:11 +0000

    init repo

---
+++ rpc-test/rpc-test-comsumer/src/main/java/com/dcy/Main.java
@@ -0,0 +1,11 @@
+package com.dcy;
+
+/**
+ * @author Kyle
+ * @date 2024/02/19
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
