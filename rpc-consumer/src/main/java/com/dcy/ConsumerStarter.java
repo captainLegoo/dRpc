31867659eb675b19c11fe4057203cd07e71a2f9b
captaindeng <captaindeng12138@gmail.com>
2024-02-19 16:00:46 +0000

    Configure related dependencies

---
+++ rpc-consumer/src/main/java/com/dcy/ConsumerStarter.java
@@ -0,0 +1,8 @@
+package com.dcy;
+
+/**
+ * @author Kyle
+ * @date 2024/02/19
+ */
+public class ConsumerStarter {
+}
--- rpc-test/rpc-test-provider/src/main/java/com/dcy/Main.java
+++ rpc-test/rpc-test-provider/src/main/java/com/dcy/Main.java
@@ -6,6 +6,6 @@ package com.dcy;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new ProviderStarter();
     }
 }
