937969c1b740c9b0e5e5cb21de6bc57e321cb2b5
captaindeng <captaindeng12138@gmail.com>
2024-03-09 15:59:15 +0000

    change property

--- rpc-core/src/main/java/com/dcy/rpc/config/RegistryConfig.java
+++ rpc-core/src/main/java/com/dcy/rpc/config/RegistryConfig.java
@@ -1,22 +1,17 @@
 package com.dcy.rpc.config;
 
+import com.dcy.rpc.enumeration.RegistryCenterEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * @author Kyle
  * @date 2024/02/20
  */
+@Data
+@AllArgsConstructor
 public class RegistryConfig {
-    private String registryAddress;
-    private String registryCenter;
-    private String host;
-    private int port;
-
-    public RegistryConfig(String registryAddress) {
-        this.registryAddress = registryAddress;
-    }
-
-    public RegistryConfig(String registryCenter, String host, int port) {
-        this.registryCenter = registryCenter;
-        this.host = host;
-        this.port = port;
-    }
+    private final RegistryCenterEnum registryCenterEnum;
+    private final String host;
+    private final int port;
 }
