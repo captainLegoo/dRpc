064ab0f9fe65c3ec58d8b734de0eecb47dca8c30
captaindeng <captaindeng12138@gmail.com>
2024-03-26 20:57:17 +0000

    change controller package name

--- rpc-test/rpc-test-spring-provider/src/main/java/com/dcy/controller/UserController.java
+++ rpc-test/rpc-test-spring-provider/src/main/java/com/dcy/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.dcy.Controller;
+package com.dcy.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
