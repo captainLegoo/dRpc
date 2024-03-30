bfa64681f7e3ef50fee05e85a26740181e65dffb
captaindeng <captaindeng12138@gmail.com>
2024-03-30 15:53:02 +0000

    implement api UserService as provide of service

---
+++ rpc-test/rpc-test-spring-provider/src/main/java/com/dcy/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.dcy.service.impl;
+
+import com.dcy.rpc.UserService;
+import com.dcy.rpc.annotation.RpcService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Kyle
+ * @date 2024/03/30
+ */
+@Service
+@RpcService
+public class UserServiceImpl implements UserService {
+    @Override
+    public String sayHello(String name) {
+        return "Hello spring rpc provider";
+    }
+}
