455933ca595f8b578fa553ff12dce4af683e3a64
captaindeng <captaindeng12138@gmail.com>
2024-03-30 15:57:26 +0000

    spring-Provider use bean to publish service

---
+++ rpc-test/rpc-test-spring-provider/src/main/java/com/dcy/config/RpcConfig.java
@@ -0,0 +1,27 @@
+package com.dcy.config;
+
+import com.dcy.rpc.bootstrap.DRpcBootstrap;
+import com.dcy.rpc.enumeration.RegistryCenterEnum;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Kyle
+ * @date 2024/03/30
+ * <p>
+ * rpc-provider config
+ */
+@Configuration
+public class RpcConfig {
+
+    @Bean
+    public DRpcBootstrap rpcProviderConfig() {
+        DRpcBootstrap.getInstance()
+                .setBootstrapName("RPC-Provider")
+                .port(9600)
+                .registry(RegistryCenterEnum.ZOOKEEPER, "192.168.64.128", 2181)
+                .scanAndPublish("com.dcy.service.impl")
+                .start();
+        return DRpcBootstrap.getInstance();
+    }
+}
