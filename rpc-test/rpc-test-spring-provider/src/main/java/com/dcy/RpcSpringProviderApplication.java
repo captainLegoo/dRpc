4d040ed35239124cb65893cd1ceb67641144f07a
captaindeng <captaindeng12138@gmail.com>
2024-03-30 15:31:17 +0000

    create module for testing spring with rpc

---
+++ rpc-test/rpc-test-spring-provider/src/main/java/com/dcy/RpcSpringProviderApplication.java
@@ -0,0 +1,13 @@
+package com.dcy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcSpringProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcSpringProviderApplication.class, args);
+    }
+
+}
