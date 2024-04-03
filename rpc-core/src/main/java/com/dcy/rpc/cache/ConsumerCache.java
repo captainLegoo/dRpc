0a0a9cb1216ddd4b06ab8e531d71ae8995801e99
captaindeng <captaindeng12138@gmail.com>
2024-04-03 21:33:25 +0100

    caching of service name and service address

--- rpc-core/src/main/java/com/dcy/rpc/cache/ConsumerCache.java
+++ rpc-core/src/main/java/com/dcy/rpc/cache/ConsumerCache.java
@@ -2,6 +2,8 @@ package com.dcy.rpc.cache;
 
 import com.dcy.rpc.loadbalancer.Loadbalancer;
 
+import java.net.InetSocketAddress;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
@@ -18,4 +20,7 @@ public class ConsumerCache {
 
     // Caching of service name and load balancing
     public static final Map<String, Loadbalancer> LOADBALANCER_MAP = new ConcurrentHashMap<>(16);
+
+    // Caching of service name and service address
+    public static final Map<String, List<InetSocketAddress>> SERVICE_ADDRESS_MAP = new ConcurrentHashMap<>(16);
 }
