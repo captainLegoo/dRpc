b0b5b96c95a47540ee115ee8f5e70b969bc38aa2
captaindeng <captaindeng12138@gmail.com>
2024-04-06 17:33:28 +0100

    impl Serialize interface

--- rpc-serialization/rpc-serialization-protostuff/src/main/java/com/dcy/rpc/serialize/protostuff/ProtostuffSerialize.java
+++ rpc-serialization/rpc-serialization-protostuff/src/main/java/com/dcy/rpc/serialize/protostuff/ProtostuffSerialize.java
@@ -1,6 +1,7 @@
 package com.dcy.rpc.serialize.protostuff;
 
 
+import com.dcy.rpc.serialize.Serialize;
 import io.protostuff.LinkedBuffer;
 import io.protostuff.ProtostuffIOUtil;
 import io.protostuff.Schema;
@@ -12,33 +13,35 @@ import io.protostuff.runtime.RuntimeSchema;
  * <p>
  * Serialization using protostuff
  */
-public class ProtostuffSerialize {
+public class ProtostuffSerialize implements Serialize {
+
     /**
      * Serialization
-     * @param t
-     * @param <T>
+     * @param object Object instance to be serialized
      * @return
      */
-    public static <T> byte[] serialize(T t){
-        Schema schema = RuntimeSchema.getSchema(t.getClass());
-        return ProtostuffIOUtil.toByteArray(t,schema,
+    @Override
+    public byte[] serializer(Object object) {
+        Schema schema = RuntimeSchema.getSchema(object.getClass());
+        return ProtostuffIOUtil.toByteArray(object, schema,
                 LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE));
 
     }
 
     /**
      * Deserialization
-     * @param bytes
-     * @param c
+     * @param bytes Byte array to be deserialized
+     * @param c     The class object of the target class
      * @param <T>
      * @return
      */
-    public static <T> T deserialize(byte []bytes,Class<T> c) {
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> c) {
         T t = null;
         try {
             t = c.newInstance();
             Schema schema = RuntimeSchema.getSchema(t.getClass());
-            ProtostuffIOUtil.mergeFrom(bytes,t,schema);
+            ProtostuffIOUtil.mergeFrom(bytes, t, schema);
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
