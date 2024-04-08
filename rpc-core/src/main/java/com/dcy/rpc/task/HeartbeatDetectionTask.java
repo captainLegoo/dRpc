ba9d5ed29088aaf4ac495b65d7d0282aa3e536e1
captaindeng <captaindeng12138@gmail.com>
2024-04-08 20:34:43 +0100

    use a class to send Heartbeat Request

--- rpc-core/src/main/java/com/dcy/rpc/task/HeartbeatDetectionTask.java
+++ rpc-core/src/main/java/com/dcy/rpc/task/HeartbeatDetectionTask.java
@@ -3,19 +3,16 @@ package com.dcy.rpc.task;
 import com.dcy.rpc.bootstrap.DRpcBootstrap;
 import com.dcy.rpc.cache.ConsumerCache;
 import com.dcy.rpc.config.GlobalConfig;
-import com.dcy.rpc.constant.MessageConstant;
-import com.dcy.rpc.entity.RequestProtocol;
-import com.dcy.rpc.enumeration.RequestTypeEnum;
-import com.dcy.rpc.netty.ConsumerNettyStarter;
-import io.netty.channel.Channel;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Kyle
@@ -41,44 +38,12 @@ public class HeartbeatDetectionTask implements Runnable{
             String serviceName = entry.getKey();
             log.debug("Start detect serviceName is -> {}", serviceName);
             List<InetSocketAddress> inetSocketAddressList = entry.getValue();
+            log.debug("Start detect inetSocketAddressList size() is -> {}", inetSocketAddressList.size());
             // list convert to list
             InetSocketAddress[] inetSocketAddressArray = inetSocketAddressList.toArray(new InetSocketAddress[0]);
             for (InetSocketAddress address : inetSocketAddressArray) {
                 // send request
-                pool.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        // encapsulate the message
-                        long requestId = globalConfig.getIdGenerator().getId();
-                        RequestProtocol requestProtocol = new RequestProtocol()
-                                .setRequestId(requestId)
-                                .setRequestType(RequestTypeEnum.HEART.getId())
-                                .setCompressType(globalConfig.getCompressType().getCompressId())
-                                .setSerializeType(globalConfig.getSerializableType().getSerializeId())
-                                .setTimeStamp(new Date().getTime())
-                                .setRequestPayload(null);
-
-                        // get available channel
-                        Channel channel = ConsumerNettyStarter.getNettyChannel(address);
-                        // create CompletableFuture and add to cache, Waiting to receive return information
-                        CompletableFuture<Object> completableFuture = new CompletableFuture<>();
-                        // send heartbeat request
-                        channel.writeAndFlush(requestProtocol);
-                        ConsumerCache.FUTURES_NAP.put(requestId, completableFuture);
-                        // get heartbeat result
-                        try {
-                            String res = (String) completableFuture.get(5, TimeUnit.SECONDS);
-                            if (!res.equals(MessageConstant.HEARTBEAT_REQUEST) || Objects.isNull(res)) {
-                                inetSocketAddressList.remove(address);
-                                log.error("Heartbeat detection failed, serviceName is -> {}, address is -> {}", serviceName, address);
-                            }
-                        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-                            inetSocketAddressList.remove(address);
-                            log.error("Heartbeat detection failed, serviceName is -> {}, address is -> {}", serviceName, address);
-                            //throw new RuntimeException(e);
-                        }
-                    }
-                });
+                pool.execute(new SendHeartbeatRequest(serviceName, address, globalConfig, inetSocketAddressList));
             }
         }
     }
