1f9d1af57515515f979e7d93078f51d5e599c574
captaindeng <captaindeng12138@gmail.com>
2024-04-11 18:29:14 +0100

    cache of zkp client of comment

--- rpc-registrycenter/rpc-registrycenter-zookeeper/src/main/java/com/dcy/rpc/cache/ZkpCache.java
+++ rpc-registrycenter/rpc-registrycenter-zookeeper/src/main/java/com/dcy/rpc/cache/ZkpCache.java
@@ -10,5 +10,6 @@ import java.util.concurrent.ConcurrentHashMap;
  * @date 2024/04/07
  */
 public class ZkpCache {
+    // cache for zkp client  key:address(127.0.0.1:2181)  value:client object
     public static final Map<String, CuratorFramework> CLIENT_CACHE = new ConcurrentHashMap<>(8);
 }
