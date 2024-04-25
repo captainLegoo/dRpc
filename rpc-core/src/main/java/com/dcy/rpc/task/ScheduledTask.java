db4b9672d775d25adcd54cbece8fc5ecd628c783
captaindeng <captaindeng12138@gmail.com>
2024-04-25 16:48:46 +0100

    sending heart-beat to provider

---
+++ rpc-core/src/main/java/com/dcy/rpc/task/ScheduledTask.java
@@ -0,0 +1,41 @@
+package com.dcy.rpc.task;
+
+import com.dcy.rpc.cache.NettyCache;
+import com.dcy.rpc.config.GlobalConfig;
+import com.dcy.rpc.listen.ListenZkpServiceAddress2;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Kyle
+ * @date 2024/04/11
+ */
+public class ScheduledTask {
+
+    private final GlobalConfig globalConfig;
+
+    public ScheduledTask(GlobalConfig globalConfig) {
+        this.globalConfig = globalConfig;
+    }
+
+
+    public void startDoingTask() {
+        //
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleWithFixedDelay(new HeartbeatDetectionTask(),
+                15,
+                2,
+                TimeUnit.SECONDS);
+
+        // listen address
+        String host = globalConfig.getRegistryConfig().getHost();
+        int port = globalConfig.getRegistryConfig().getPort();
+        String clientAddress = host + ":" +port;
+        Thread thread = new Thread(new ListenZkpServiceAddress2(clientAddress, NettyCache.PENDING_REMOVE_ADDRESS_MAP));
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+}
