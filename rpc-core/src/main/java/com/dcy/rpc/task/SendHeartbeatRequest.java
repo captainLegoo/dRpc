4c1050ad1e1914fef68213878121050098648cac
captaindeng <captaindeng12138@gmail.com>
2024-04-25 16:54:26 +0100

    add re-load balance through service address cache

--- rpc-core/src/main/java/com/dcy/rpc/task/SendHeartbeatRequest.java
+++ rpc-core/src/main/java/com/dcy/rpc/task/SendHeartbeatRequest.java
@@ -6,11 +6,13 @@ import com.dcy.rpc.config.GlobalConfig;
 import com.dcy.rpc.constant.MessageConstant;
 import com.dcy.rpc.entity.RequestProtocol;
 import com.dcy.rpc.enumeration.RequestTypeEnum;
+import com.dcy.rpc.loadbalancer.Loadbalancer;
 import com.dcy.rpc.netty.ConsumerNettyStarter;
 import io.netty.channel.Channel;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -22,6 +24,8 @@ import java.util.concurrent.TimeoutException;
 /**
  * @author Kyle
  * @date 2024/04/08
+ * <p>
+ * sending heart beat to the service address (provider)
  */
 @Slf4j
 public class SendHeartbeatRequest implements Runnable{
@@ -51,12 +55,9 @@ public class SendHeartbeatRequest implements Runnable{
 
         // get available channel
         Channel channel = ConsumerNettyStarter.getNettyChannel(address);
-        if (channel == null) {
-            //removeInvalidAddress();
-            return;
-        }
         // create CompletableFuture and add to cache, Waiting to receive return information
         CompletableFuture<Object> completableFuture = new CompletableFuture<>();
+
         try {
             // send heartbeat request
             channel.writeAndFlush(requestProtocol);
@@ -69,7 +70,6 @@ public class SendHeartbeatRequest implements Runnable{
         } catch (InterruptedException | ExecutionException | TimeoutException e) {
             removeInvalidAddress();
             log.error("Heartbeat detection failed, address is -> {}", address);
-            //throw new RuntimeException(e);
         } finally {
             // remove completableFuture
             ConsumerCache.FUTURES_NAP.remove(requestId);
@@ -77,9 +77,20 @@ public class SendHeartbeatRequest implements Runnable{
     }
 
     private void removeInvalidAddress() {
-        //inetSocketAddressList.remove(address);
-        NettyCache.CHANNEL_MAP.remove(address);
+        //NettyCache.CHANNEL_MAP.remove(address);
         boolean remove = ConsumerCache.SERVICE_ADDRESS_MAP.get(serviceName).remove(address);
-        log.debug("removeInvalidAddress remove -> {}, address -> {}", remove, address);
+        log.debug("removeInvalidAddress remove -> {}, service -> {} address -> {}", remove, serviceName, address);
+        if (remove) {
+            List<InetSocketAddress> addressList = NettyCache.PENDING_REMOVE_ADDRESS_MAP.computeIfAbsent(serviceName, k -> new ArrayList<>());
+            addressList.add(address);
+            // re-loadBalance
+            reloadBalance(serviceName);
+        }
+    }
+
+    private void reloadBalance(String serviceName) {
+        Loadbalancer loadbalancer = ConsumerCache.LOADBALANCER_MAP.get(serviceName);
+        loadbalancer.reloadBalance(ConsumerCache.SERVICE_ADDRESS_MAP.get(serviceName));
+        log.debug("The serviceName {} has been successfully reload balanced...", serviceName);
     }
 }
