3a5266e3f39b118f26b940525b020346f8f1ee13
captaindeng12138 <captaindeng12138@gmail.com>
2024-05-10 16:47:39 +0100

    pattern of IP Address

---
+++ rpc-registrycenter/rpc-registrycenter-zookeeper/src/main/java/com/dcy/rpc/constant/IPAddressConstant.java
@@ -0,0 +1,14 @@
+package com.dcy.rpc.constant;
+
+/**
+ * @author Kyle
+ * @date 2024/05/10
+ */
+public class IPAddressConstant {
+    public static final String IPADDRESS_PATTERN =
+            "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+}
