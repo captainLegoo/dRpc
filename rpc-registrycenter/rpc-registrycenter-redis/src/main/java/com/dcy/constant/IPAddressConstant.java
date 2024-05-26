8f0696f93d1e87f9ebacfa0be35782b889f4bf0d
captaindeng12138 <captaindeng12138@gmail.com>
2024-05-26 16:20:32 +0100

    ip address pattern rule

---
+++ rpc-registrycenter/rpc-registrycenter-redis/src/main/java/com/dcy/constant/IPAddressConstant.java
@@ -0,0 +1,15 @@
+package com.dcy.constant;
+
+/**
+ * @author Kyle
+ * @date 2024/05/10
+ * @description ip address pattern rule
+ */
+public class IPAddressConstant {
+    public static final String IPADDRESS_PATTERN =
+            "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+}
