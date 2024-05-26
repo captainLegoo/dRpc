620e5a49cc6cfcc553e9dc76d3ac0313134bafea
captaindeng12138 <captaindeng12138@gmail.com>
2024-05-26 16:20:26 +0100

    redis connect constant

---
+++ rpc-registrycenter/rpc-registrycenter-redis/src/main/java/com/dcy/constant/ConnectConstant.java
@@ -0,0 +1,12 @@
+package com.dcy.constant;
+
+/**
+ * @author Kyle
+ * @date 2024/05/26
+ * @description connect constant
+ */
+public class ConnectConstant {
+    public static final String NAMESPACE = "drpc_";
+    public static final String NODE_DEFAULT_PATH = "metadata_provider_";
+
+}
