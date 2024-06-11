ae9e327016c3eb6e06623c30f4077712d26a8b57
captaindeng12138 <captaindeng12138@gmail.com>
2024-06-11 20:55:00 +0100

    style: delete unused code

--- rpc-core/src/main/java/com/dcy/rpc/task/ScheduledTask.java
+++ rpc-core/src/main/java/com/dcy/rpc/task/ScheduledTask.java
@@ -33,28 +33,13 @@ public class ScheduledTask {
 
     private final GlobalConfig globalConfig;
     private final ScheduledExecutorService scheduler;
-    //private final ThreadPoolExecutor pool;
 
     public ScheduledTask(GlobalConfig globalConfig) {
         this.globalConfig = globalConfig;
         this.scheduler = Executors.newSingleThreadScheduledExecutor();
-        //this.pool = new ThreadPoolExecutor(
-        //        10, // Number of core threads
-        //        10, // Maximum number of threads
-        //        0L, // The maximum time an idle thread waits for a new task
-        //        TimeUnit.MILLISECONDS,
-        //        new LinkedBlockingQueue<>(), // task queue
-        //        Executors.defaultThreadFactory(),
-        //        new ThreadPoolExecutor.AbortPolicy() // Rejection strategy after both queue and thread pool are full
-        //);
     }
 
     public void startDoingTask() {
-        //scheduler.scheduleWithFixedDelay(new HeartbeatDetectionTask(),
-        //        15,
-        //        5,
-        //        TimeUnit.SECONDS);
-
         scheduler.scheduleWithFixedDelay(new checkPendingOnlineAddress(),
                 12,
                 3,
@@ -92,68 +77,6 @@ public class ScheduledTask {
                     5,
                     TimeUnit.SECONDS
             );
-        }
-
-    }
-
-    private class HeartbeatDetectionTask implements Runnable {
-
-        private final GlobalConfig globalConfig = DRpcBootstrap.getInstance().getGlobalConfig();
-
-        @Override
-        public void run() {
-            log.debug("Heartbeat Detection Time -> 【{}】...", new Date());
-
-            try {
-                //checkPendingOnlineAddress();
-                //heartbeatDetectionTask();
-            } catch (Exception e) {
-                log.error("HeartbeatDetectionTask error: {}", e.getMessage(), e);
-            }
-        }
-
-        private void checkPendingOnlineAddress() {
-            NettyCache.PENDING_ADD_ADDRESS_MAP.forEach((serviceName, inetSocketAddressList) -> {
-                List<InetSocketAddress> addressList = ConsumerCache.SERVICE_ADDRESS_MAP.computeIfAbsent(serviceName, k -> new ArrayList<>());
-
-                // Iterate over each address in PENDING_ADD_ADDRESS_MAP and process them
-                Iterator<InetSocketAddress> iterator = inetSocketAddressList.iterator();
-                while (iterator.hasNext()) {
-                    InetSocketAddress address = iterator.next();
-                    Channel channel = ConsumerNettyStarter.getNettyChannel(address);
-                    log.info("HeartbeatDetectionTask checking channel -> {}", channel);
-
-                    // If the address is not in addressList, add it and reload the load balancer if it exists
-                    if (!addressList.contains(address)) {
-                        addressList.add(address);
-                        Loadbalancer loadbalancer = ConsumerCache.LOADBALANCER_MAP.get(serviceName);
-                        if (loadbalancer != null) {
-                            loadbalancer.reloadBalance(addressList);
-                        }
-                        log.debug("HeartbeatDetectionTask reloadBalance addressList -> {}", addressList);
-
-                        // Remove processed addresses from PENDING_ADD_ADDRESS_MAP
-                        iterator.remove();
-                    }
-                }
-
-                // If the list in PENDING_ADD_ADDRESS_MAP is empty, delete the entire entry
-                if (inetSocketAddressList.isEmpty()) {
-                    NettyCache.PENDING_ADD_ADDRESS_MAP.remove(serviceName);
-                }
-            });
-        }
-
-        private void heartbeatDetectionTask() {
-            if (ConsumerCache.SERVICE_ADDRESS_MAP.isEmpty()) return;
-            ConsumerCache.SERVICE_ADDRESS_MAP.forEach((serviceName, inetSocketAddressList) -> {
-                log.debug("Start detect serviceName is -> 【{}】...", serviceName);
-                log.debug("Start detect inetSocketAddressList is -> 【{}】...", inetSocketAddressList);
-                for (InetSocketAddress address : inetSocketAddressList) {
-                    // Submit tasks to the thread pool for execution
-                    GlobalThreadPool.pool.execute(new SendHeartbeatRequest(serviceName, address, globalConfig));
-                }
-            });
         }
     }
 
